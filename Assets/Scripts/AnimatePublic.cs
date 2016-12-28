3b65cc8db881ed3b4a735ce375a445335953696f
Dani <dani_rl94@hotmail.com>
2016-12-28 15:54:18 +0100

    Import from Quidditch

---
+++ Assets/Scripts/AnimatePublic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatePublic : MonoBehaviour {
+
+    //public float AccelerometerUpdateInterval = 1.0f / 100.0f;
+    //public float LowPassKernelWidthInSeconds = 0.001f;
+    private float LowPassFilterFactor = 0.8f;
+    private Vector3 lowPassValue = Vector3.zero;
+    private Gyroscope gyro; 
+
+	// Use this for initialization
+	void Start () {
+        Input.gyro.enabled = true;
+        gyro = Input.gyro;
+	}
+
+
+    Vector3 lowpass()
+    {
+        //float LowPassFilterFactor = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds; // tweakable
+        lowPassValue = Vector3.Lerp(lowPassValue, Input.acceleration, LowPassFilterFactor);
+        return lowPassValue;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        AccelerationEvent[] events = Input.accelerationEvents;
+        for (int i = 0; i < events.Length; i++)
+        {
+            Debug.LogError("Accel: " + events[i].acceleration);
+        }
+        Debug.LogError("Gyro accel: " + gyro.userAcceleration + " - Rotation: " + Quaternion.ToEulerAngles(gyro.attitude));
+	}
+}
---
+++ Assets/Scripts/StartCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCamera : MonoBehaviour {
+    public GameObject ListaPasos, Camera;
+
+    [HideInInspector]
+    public bool activated = true;
+    private Transform[] coordenadas;
+    private Vector3 step;
+    private int iteracion = 0;
+    private float elapsedTime = 2.5f;
+	// Use this for initialization
+	void Start () {
+        coordenadas = ListaPasos.GetComponentsInChildren<Transform>();
+        step = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (activated) {
+            elapsedTime -= Time.deltaTime;
+            if (elapsedTime < 0) {
+                iteracion++;
+                if (iteracion < coordenadas.Length) {
+                    elapsedTime = 2f;
+                    step = (coordenadas[iteracion].position - coordenadas[iteracion - 1].position) / elapsedTime;
+                }
+                else activated = false;
+            }
+            else {
+                Camera.transform.position += step * Time.deltaTime;
+            }
+        }
+	}
+}
