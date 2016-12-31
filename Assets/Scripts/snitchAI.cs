04cbbd80c327b115d48ba592a6c7a092efc4bd87
Alex Osés Laza <aol.treballs@gmail.com>
2016-12-31 19:19:44 +0100

    now snitch won't go out of the boundaries when scaping player

--- Assets/Scripts/snitchAI.cs
+++ Assets/Scripts/snitchAI.cs
@@ -6,7 +6,8 @@ public class snitchAI : MonoBehaviour {
 
 	public float speed;
 	public Camera playerCamera;
-
+	public Vector3 Max;
+	public Vector3 Min;
 	private Vector3 playerPos;
 
 	// Use this for initialization
@@ -18,11 +19,22 @@ public class snitchAI : MonoBehaviour {
 		float step = speed * Time.deltaTime;
 		Vector3 heading = playerPos - transform.position;
 		// divisions are expensive.
-		transform.position += (step * heading) / heading.magnitude;
+		transform.position -= (step * heading) / heading.magnitude;
+	}
+	//Check if snitch is inside the pitch
+	bool insideBoundaries(){
+		Vector3 snitchPos = transform.position;
+		if (snitchPos.x + 1 > Max.x || snitchPos.x - 1 < Min.x)
+			return false;
+		if (snitchPos.y + 1 > Max.y || snitchPos.y - 1 < Min.y)
+			return false;
+		if (snitchPos.z + 1> Max.z || snitchPos.z - 1 < Min.z)
+			return false;
+		return true;
 	}
 	// Update is called once per frame
 	void Update () {
 		playerPos = playerCamera.transform.position;
-		moveAway ();
+		if (insideBoundaries()) moveAway ();
 	}
 }
