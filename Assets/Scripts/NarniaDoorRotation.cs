f67eaa402d59fe6dcf2ce0c4312b88eb0b656fbe
Alex Osés Laza <aol.treballs@gmail.com>
2016-12-31 12:16:50 +0100

    Scene created, doors located, animation script for doors done

---
+++ Assets/Scripts/NarniaDoorRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarniaDoorRotation : MonoBehaviour {
+	private Vector3 hingePoint;
+	// Use this for initialization
+	void Start () {
+		hingePoint = this.transform.GetChild(0).position;
+		rotateDoor();
+	}
+	void rotateDoor(){
+		transform.RotateAround (hingePoint, new Vector3 (0, 1, 0), 90);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
