f888fda0fccc874bcead6e844569f37d8d81b21a
Alex Osés Laza <aol.treballs@gmail.com>
2016-12-31 18:47:29 +0100

    snitch runs away from player without taking any other considerations.

---
+++ Assets/Scripts/snitchAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snitchAI : MonoBehaviour {
+
+	public float speed;
+	public Camera playerCamera;
+
+	private Vector3 playerPos;
+
+	// Use this for initialization
+	void Start () {
+		playerPos = playerCamera.transform.position;
+	}
+
+	void moveAway(){
+		float step = speed * Time.deltaTime;
+		Vector3 heading = playerPos - transform.position;
+		// divisions are expensive.
+		transform.position += (step * heading) / heading.magnitude;
+	}
+	// Update is called once per frame
+	void Update () {
+		playerPos = playerCamera.transform.position;
+		moveAway ();
+	}
+}
