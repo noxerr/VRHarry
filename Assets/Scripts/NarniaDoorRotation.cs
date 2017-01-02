77c092ae0ab62d91bf72cd2a835b7d33ec87c5e2
Dani <dani_rl94@hotmail.com>
2017-01-02 19:48:33 +0100

    added circular slider to open the doors and sky

--- Assets/Scripts/NarniaDoorRotation.cs
+++ Assets/Scripts/NarniaDoorRotation.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NarniaDoorRotation : MonoBehaviour {
     public float secsForEachAngle = 0.008f;
+    private float secsToLoadScene = 1.5f;
+    public GameObject slider;
 	private Vector3 hingePoint;
     private bool moveDoor;
     private float timeMoving;
     private float angleStep;
     private float angleFinal;
+    private bool loadSlider = false;
+    private float secsToLoad;
+    private Image sliderImage;
 	// Use this for initialization
 	void Start () {
         moveDoor = false;
 		hingePoint = this.transform.GetChild(0).position;
         angleFinal = 0;
         angleStep = 0;
+        secsToLoad = secsToLoadScene;
+        sliderImage = slider.GetComponent<Image>();
 		//rotateDoor(90);
 	}
 	public void rotateDoor(float angle = 90){
@@ -23,10 +31,24 @@ public class NarniaDoorRotation : MonoBehaviour {
         //if (transform.localEulerAngles.y > 90 && transform.localEulerAngles.y != 0
         timeMoving = Mathf.Abs(angleStep * secsForEachAngle);
         angleFinal = angle;
+        if (angle == 90)
+        {
+            loadSlider = true;
+            slider.SetActive(true);
+        }
+        else
+        {
+            loadSlider = false;
+            secsToLoad = secsToLoadScene;
+            slider.SetActive(false);
+            sliderImage.fillAmount = 0;
+        }
         moveDoor = true;
 	}
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+    {
+        #region moveDoor
         if (moveDoor)
         {
             if (timeMoving > 0)
@@ -49,5 +71,14 @@ public class NarniaDoorRotation : MonoBehaviour {
             }
             else moveDoor = false;
         }
-	}
+        #endregion
+
+        #region slider
+        if (loadSlider)
+        {
+            if (secsToLoad > 0) secsToLoad -= Time.deltaTime;
+            sliderImage.fillAmount += 1.0f / secsToLoadScene * Time.deltaTime;// *secsToLoad;
+        }
+        #endregion
+    }
 }
