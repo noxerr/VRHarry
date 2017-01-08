a0fabc443275e361296ab4a249aab350d7a0f294
Dani <dani_rl94@hotmail.com>
2017-01-08 17:07:25 +0100

    added tutorialScene v2.0

---
+++ Assets/Scripts/AllLogics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllLogics : MonoBehaviour {
+    [HideInInspector]
+    public bool fadeInFinished = false;
+    [HideInInspector]
+    public bool snitchCatched = false;
+}
--- Assets/Scripts/snitchAI.cs
+++ Assets/Scripts/snitchAI.cs
@@ -7,12 +7,20 @@ public class snitchAI : MonoBehaviour {
 	public float speed;
 	public Camera playerCamera;
 	public GameObject Path;
-	public float distanceToPoint;
+	public float distanceToPoint = 1f;
+    [HideInInspector]
+    public bool isActive = false, isAnimating = false;
+    [HideInInspector]
+    public AllLogics logic;
 	private Transform[] points;
 	private int currentPos;
 	private Vector3 Max = new Vector3(247,21,369);
 	private Vector3 Min = new Vector3(-23,-59,228);
 	private Vector3 playerPos;
+    private float distPlayerSnitch;
+    private Vector3 snitchPos;
+    private bool animatedWings = false;
+    private float step;
 
 	/*Good v0.1 Min Max positions:
 		Max:	247		21		369
@@ -26,19 +34,19 @@ public class snitchAI : MonoBehaviour {
 	}
 
 	void moveAway(){
-		float step = speed * Time.deltaTime;
+		step = speed * Time.deltaTime;
 		Vector3 heading = playerPos - transform.position;
 		// divisions are expensive.
 		transform.position -= (step * heading) / heading.magnitude;
 	}
 	//Check if snitch is inside the pitch
 	bool insideBoundaries(){
-		Vector3 snitchPos = transform.position;
-		if (snitchPos.x + 1 > Max.x || snitchPos.x - 1 < Min.x)
+		snitchPos = transform.position;
+		if (snitchPos.x > Max.x || snitchPos.x < Min.x)
 			return false;
-		if (snitchPos.y + 1 > Max.y || snitchPos.y - 1 < Min.y)
+		if (snitchPos.y > Max.y || snitchPos.y < Min.y)
 			return false;
-		if (snitchPos.z + 1> Max.z || snitchPos.z - 1 < Min.z)
+		if (snitchPos.z > Max.z || snitchPos.z < Min.z)
 			return false;
 		return true;
 	}
@@ -49,24 +57,34 @@ public class snitchAI : MonoBehaviour {
 			currentPos = 1;
 	}
 	void followPath(){
-		Vector3 position = points [currentPos].position;
-		if (Vector3.Distance (position, transform.position) < distanceToPoint)
+		if (Vector3.Distance (points [currentPos].position, transform.position) < distanceToPoint)
 			getNextPoint ();
 		//transform.position = Vector3.MoveTowards (transform.position, position, speed * Time.deltaTime);
 
-		float step = speed * Time.deltaTime;
-		Vector3 heading = position - transform.position;
+		step = speed * Time.deltaTime;
+        Vector3 heading = points[currentPos].position - transform.position;
 		// divisions are expensive.
 		transform.position += (step * heading) / heading.magnitude;
 	}
 	// Update is called once per frame
-	void Update () {
-		playerPos = playerCamera.transform.position;
-		bool isSafeFromPlayer = Vector3.Distance (playerPos, transform.position) > 10;
-		if (isSafeFromPlayer)
-			followPath ();
-		else if (insideBoundaries ())
-			moveAway ();
-		//if (insideBoundaries()) moveAway ();
+	void FixedUpdate () {
+        if (isActive)
+        {
+            distPlayerSnitch = Vector3.Distance(playerCamera.transform.position, transform.position);
+            if (distPlayerSnitch > 10)
+                followPath();
+            else if (distPlayerSnitch < 0.6f)
+            {
+                if (logic != null) logic.snitchCatched = true;
+                isActive = false;
+            }
+            else if (insideBoundaries())
+                moveAway();
+            //if (insideBoundaries()) moveAway ();
+        }
+        /*else if (isAnimating)
+        {
+
+        }*/
 	}
 }
